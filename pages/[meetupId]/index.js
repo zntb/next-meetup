d4051dcc9aebd89c71f7d18c2945aff3751e07d2
zntb <zenetibi@yahoo.com>
2024-01-08 09:47:59 +0200

    Add Custom Components & CSS Modules

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from '@/components/meetups/MeetupDetail';
+
+/* eslint-disable @next/next/no-img-element */
+function MeetupDetails() {
+  return (
+    <MeetupDetail
+      image="https://cdn.nwmgroups.hu/s/img/i/1809/20180908munchen-marienplatz-foter-ovaros.jpg"
+      title="A First Meetup"
+      address="Some address 5, 12345 Some City"
+      description="The meetup description."
+    />
+  );
+}
+export default MeetupDetails;
