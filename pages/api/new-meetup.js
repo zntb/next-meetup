2f20fea5191535f0f79adb906aabb888cffbfe5f
zntb <zenetibi@yahoo.com>
2024-01-09 10:14:02 +0200

    Working with MongoDB

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,8 +1,27 @@
-function handler(req, res) {
+import { MongoClient } from 'mongodb';
+
+// import { MONGO_URI } from '../../.env';
+
+async function handler(req, res) {
   if (req.method === 'POST') {
     const data = req.body;
 
     const { title, image, address, description } = data;
+    try {
+      const client = await MongoClient.connect(process.env.MONGO_URI);
+      const db = client.db();
+
+      const meetupsCollection = db.collection('meetups');
+      const result = await meetupsCollection.insertOne(data);
+
+      console.log(result);
+
+      client.close();
+
+      res.status(201).json({ message: 'Meetup inserted.' });
+    } catch (error) {
+      throw new Error('Connection error');
+    }
   }
 }
 export default handler;
