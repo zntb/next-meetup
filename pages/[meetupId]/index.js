b1c7ede5cbda87f6e7fe562e094eb706be5f2c79
zntb <zenetibi@yahoo.com>
2024-01-09 08:25:41 +0200

    Preparing Paths with getStaticPaths & Working With Fallback Pages

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,23 +1,44 @@
-import MeetupDetail from '@/components/meetups/MeetupDetail';
+import MeetupDetail from '../../components/meetups/MeetupDetail';
 
-/* eslint-disable @next/next/no-img-element */
 function MeetupDetails(props) {
-  return <MeetupDetail meetupData={props.meetupData} />;
+  return <MeetupDetail {...props.meetupData} />;
+}
+
+export async function getStaticPaths() {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupId: 'm1',
+        },
+      },
+      {
+        params: {
+          meetupId: 'm2',
+        },
+      },
+    ],
+  };
 }
 
 export async function getStaticProps(context) {
+  // fetch data for a single meetup
+
   const meetupId = context.params.meetupId;
 
   console.log(meetupId);
 
   return {
-    meetupData: {
-      id: meetupId,
-      image:
-        'https://cdn.nwmgroups.hu/s/img/i/1809/20180908munchen-marienplatz-foter-ovaros.jpg',
-      title: 'A First Meetup',
-      address: 'Some address 5, 12345 Some City',
-      description: 'The meetup description.',
+    props: {
+      meetupData: {
+        image:
+          'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
+        id: meetupId,
+        title: 'First Meetup',
+        address: 'Some Street 5, Some City',
+        description: 'This is a first meetup',
+      },
     },
   };
 }
